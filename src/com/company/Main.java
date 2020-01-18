73c1c788c7c913dd8dbf865cc41b00fdc382fffb
StoryTei11er <48697634+StoryTei11er@users.noreply.github.com>
2020-01-18 19:35:05 +0200

    Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        Square square = new Square();
+        Rectangle rectangle = new Rectangle();
+        Circle circle = new Circle();
+
+        triangle.area();
+        square.area();
+        rectangle.area();
+        circle.area();
+    }
+
+}
